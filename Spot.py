40a2680ff83f5ba3b664ee146df3dd1319b1dcf9
Jeremy Breese <jbreese99@gmail.com>
2021-06-20 18:51:34 -0500

    Working WASD movements and stand/sit

---
+++ Spot.py
@@ -0,0 +1,118 @@
+#Stephen Bowen 2021
+#System modules
+import time
+
+# Boston Dynamics modules
+from bosdyn.api.spot import robot_command_pb2 as spot_command_pb2
+from bosdyn.client.estop import EstopEndpoint, EstopKeepAlive, EstopClient
+from bosdyn.client.robot_state import RobotStateClient
+from bosdyn.client.robot_command import RobotCommandBuilder, RobotCommandClient, blocking_stand
+from bosdyn.client.lease import LeaseClient, LeaseKeepAlive
+import bosdyn.client.util
+
+
+#Custom modules
+from Estop import Estop
+
+class Spot():
+
+    def __init__(self, username, password, spotIP):
+        # Gather and assign credntials and Spot's IP
+        self.__username = username
+        self.__password = password
+        self.spotIP = spotIP
+        
+        # Set power state for Spot
+        self._isPoweredOn = False
+
+    # Authenticate user and build required interfaces
+    def auth(self):
+        try:
+            # Create robot object
+            sdk = bosdyn.client.create_standard_sdk('Dug')
+            self._robot = sdk.create_robot(self.spotIP)
+            self._robot.authenticate(self.__username, self.__password)
+
+            # Create estop client for the robot
+            estop_client = self._robot.ensure_client(EstopClient.default_service_name)
+
+            # Create estop endpoint
+            self.estop = Estop(estop_client, 60, 'Dug estop')
+
+            # Create robot state, command, and lease clients
+            self._state_client = self._robot.ensure_client(RobotStateClient.default_service_name)
+
+            self._robot_command_client = self._robot.ensure_client(RobotCommandClient.default_service_name)
+            self._lease_client = self._robot.ensure_client(LeaseClient.default_service_name)
+
+            # Acquire and retain lease and robot id
+            self._lease = self._lease_client.acquire()
+
+            # Construct our lease keep-alive object, which begins RetainLease calls in a thread.
+            self._lease_keepalive = LeaseKeepAlive(self._lease_client)
+            self._robot_id = self._robot.get_id()
+            
+            return True
+        except:
+            print("EXCEPTION")
+            return False
+
+    # Power Spot on/off
+    def togglePower(self):
+        try:
+            if self._isPoweredOn:
+                self._robot.power_off()
+                self._isPoweredOn = False
+            else:
+                self._robot.power_on()
+                self._isPoweredOn = True
+            return self._isPoweredOn
+        except:
+            return self._isPoweredOn
+
+    # Trigger EStop
+    def eStop(self):
+        self.estop.stop()
+        print("STOPPING")
+
+    # Clear EStop
+    def clearEStop(self):
+        status = self.estop.allow()
+        try: 
+            self._robot.power_on()
+            return status
+        except:
+            return status
+
+    # End connection to robot
+    def endConnection(self):
+        # Return lease and end Estop coverage
+        self._lease_client.return_lease(self._lease)
+        self._lease_keepalive.shutdown()
+        self._lease_keepalive = None
+        self.estop.estop_keep_alive.shutdown()
+        return True
+
+
+    # Receive, parse, and execute a command as a string
+    def genericMovement(self, request):
+        # Set the default speed and timing for executing velocity commands
+        VELOCITY_BASE_SPEED = 2.0  # m/s
+        VELOCITY_BASE_ANGULAR = 0.8  # rad/sec
+        VELOCITY_CMD_DURATION = 0.6  # seconds
+        print("HI")
+        requestLibrary = {
+        'sit': RobotCommandBuilder.synchro_sit_command(params=spot_command_pb2.MobilityParams(locomotion_hint=spot_command_pb2.HINT_AUTO, stair_hint=0)), # SIT
+        'stand': RobotCommandBuilder.synchro_stand_command(params=spot_command_pb2.MobilityParams(locomotion_hint=spot_command_pb2.HINT_AUTO, stair_hint=0)), # STAND
+        'W': RobotCommandBuilder.synchro_velocity_command(v_x=VELOCITY_BASE_SPEED, v_y=0, v_rot=0), # FORWAWRD
+        'A': RobotCommandBuilder.synchro_velocity_command(v_x=0, v_y=VELOCITY_BASE_SPEED, v_rot=0), # LEFT
+        'S': RobotCommandBuilder.synchro_velocity_command(v_x=-VELOCITY_BASE_SPEED, v_y=0, v_rot=0), # BACKWARD
+        'D': RobotCommandBuilder.synchro_velocity_command(v_x=0, v_y=-VELOCITY_BASE_SPEED, v_rot=0), # RIGHT
+        'Q': RobotCommandBuilder.synchro_velocity_command(v_x=0,v_y=0,v_rot=VELOCITY_BASE_ANGULAR), # TURN LEFT
+        'E': RobotCommandBuilder.synchro_velocity_command(v_x=0,v_y=0,v_rot=-VELOCITY_BASE_ANGULAR) # TURN RIGHT
+    }
+        try:
+            self._robot_command_client.robot_command_async(command=requestLibrary[request],end_time_secs=time.time()+VELOCITY_CMD_DURATION)
+            return True
+        except:
+            return False
--- spotCommands.py
+++ spotCommands.py
@@ -1,9 +1,25 @@
 import Estop
+from Spot import Spot
 import sys
+import bosdyn.client
+import bosdyn.client.lease
+import bosdyn.client.util
+import bosdyn.geometry
+
+from bosdyn.client.image import ImageClient
+from bosdyn.client.robot_command import RobotCommandBuilder, RobotCommandClient, blocking_stand
 
 def main(argv):
-    isPoweredOn = False
-    robot = initializeSpot(argv)
+    # username = str(sys.argv[2])
+    # password = str(sys.argv[4])
+    # hostname = str(sys.argv[5])
+    print("HI")
+    username = 'user'
+    password = 'h6mvzmpghunc'
+    hostname = '192.168.80.3'
+    robot = Spot(username, password, hostname)
+    robot.auth()
+
     if(robot == False):
         print("Error while initializing Spot. Exiting...")
         sys.exit()
@@ -11,80 +27,37 @@ def main(argv):
     print("Spot command.\n")
 
     while(True):
-        command = str(input("Enter a command (commands must start with spot) or enter 'end' to stop commanding spot: "))
+        command = (str(input("Enter a command (commands must start with spot) or enter 'end' to stop commanding spot: "))).lower()
         print("\n")
 
-        command = command.lower()
+        # command = command.lower()
 
         if(command == "stop" or command == "estop"):
-            spotEstop()
+            spotEstop(robot)
             break
         elif(command == "end"):
             spotShutdown(robot)
             break
         elif(command == "power"):
-            isPoweredOn = togglePower(robot, isPoweredOn)
+            togglePower(robot)
         elif "spot" not in command:
             print("Command does not begin with spot. Spot doesn't know you're talking to him.\n")
         else:
-            selectCommand(command, robot, isPoweredOn)
+            selectCommand(command, robot)
 
-def initializeSpot(argv):
-    username = str(sys.argv[1])
-    password = str(sys.argv[2])
-    hostname = str(sys.argv[3])
-    
-    try:
-        # Create robot object
-        sdk = bosdyn.client.create_standard_sdk('Spot Voice Recognition')
-        robot = sdk.create_robot(hostname)
-        robot.authenticate(username, password)
 
-        # Create estop client for the robot
-        estop_client = robot.ensure_client(EstopClient.default_service_name)
+def togglePower(robot):
+    robot.togglePower()
 
-        # Create estop endpoint
-        estop = Estop(estop_client, 60, 'Estop')
-
-        # Create robot state, command, and lease clients
-        state_client = robot.ensure_client(RobotStateClient.default_service_name)
-        command_client = robot.ensure_client(RobotCommandClient.default_service_name)
-        lease_client = robot.ensure_client(LeaseClient.default_service_name)
-
-        
-        # Construct our lease keep-alive object, which begins RetainLease calls in a thread.
-        lease_keepalive = LeaseKeepAlive(lease_client)
-
-        # Acquire and retain lease and robot id
-        lease = lease_client.acquire()
-        
-        return robot
-    except:
-        return False
-
-def togglePower(robot, isPoweredOn):
-    try:
-        if(isPoweredOn):
-            robot.power_off()
-            isPoweredOn = False
-        else:
-            robot.power_on()
-            isPoweredOn = True
-        return isPoweredOn
-    except:
-        return isPoweredOn
-
-def spotEstop():
+def spotEstop(robot):
     print("Spot EMERGENCY stop\n")
+    robot.eStop()
 
 def spotShutdown(robot):
     print("Spot shutdown\n")
-    robot.lease_client.return_lease(robot.lease)
-    robot.lease_keepalive.shutdown()
-    robot.lease_keepalive = None
-    robot.estop.estop_keep_alive.shutdown()
+    robot.endConnection()
 
-def selectCommand(command, robot, isPoweredOn):
+def selectCommand(command, robot):
     command = command.replace("spot ", "")
     print("Command: " + command + "\n")
     if "forward" in command:
@@ -102,24 +75,34 @@ def selectCommand(command, robot, isPoweredOn):
 
 def spotForward(robot):
     print("Spot moving forward\n")
+    robot.genericMovement('W')
 
 def spotBackward(robot):
     print("Spot moving backward\n")
+    robot.genericMovement('S')
+
 
 def spotRight(robot):
-    print("Spot turning left\n")
+    print("Spot turning right\n")
+    robot.genericMovement('D')
+
 
 def spotLeft(robot):
-    print("Spot turning right\n")
+    print("Spot turning left\n")
+    robot.genericMovement('A')
+
 
 def spotSit(robot):
     print("Spot sitting\n")
+    robot.genericMovement('sit')
+
 
 def spotStand(robot):
     print("Spot standing\n")
-    blocking_stand(command_client, timeout_sec=10)
+    robot.genericMovement('stand')
 
 
 if __name__ == '__main__':
     if not main(sys.argv[1:]):
         sys.exit(1)
+# main()
